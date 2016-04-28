22f5b374c2e15220b3b7d7168e8ea5ae9c230803
Luis Carlos <luisca_jl@hotmail.com>
2016-04-28 11:55:13 +0200

    Src Frost

--- proyectoangular2/src/app/register/register.service.ts
+++ proyectoangular2/src/app/register/register.service.ts
@@ -1,12 +1,15 @@
 import {RouteConfig, ROUTER_DIRECTIVES, RouteParams, Router} from 'angular2/router';
 import {Injectable} from 'angular2/core';
 import {register} from './register.model';
+import {withObserver} from '../utils';
 
 @Injectable()
 export class RegisterService{
+
   private registeruser = [
       new register('Luis Carlos','1234','1234','luisca@gmail.com'),
       new register('Paco','12345','12345','paco@gmail.com'),
+      new register('frost','1234','12345','paco@gmail.com'),
 
     ];
   getUsers(){
@@ -18,10 +21,9 @@ export class RegisterService{
     console.log(this.registeruser);
   }
 
-  getUser(nombre : string){
-    for (let i = 0; i < this.registeruser.length; i++) {
-        if(this.registeruser[i].nombre==nombre){return this.registeruser[i]};
-    }
+  getUser(nombre: string) {
+    let user = this.registeruser.filter(h => h.nombre === nombre)[0]
+    return withObserver(new register(user.nombre, user.contrasenia, user.repiticioncontrasenia, user.correousuario));
   }
 
 
